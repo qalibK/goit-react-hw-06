1a9a1e0154ab66ee474a20ec1bfa856f7482f694
qalibK <bvomusak01@gmail.com>
2024-06-19 20:50:20 +0200

    created validation for form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,14 +2,10 @@ import { Formik, Field, ErrorMessage } from "formik";
 import css from "./ContactForm.module.css";
 import * as Yup from "yup";
 import { useDispatch, useSelector } from "react-redux";
-import { useState } from "react";
 import { addContact } from "../../redux/contactSlice";
 
 export default function ContactForm() {
   const contacts = useSelector((state) => state.contacts.contacts);
-  const [name, setName] = useState("");
-  const [number, setNumber] = useState("");
-
   const dispatch = useDispatch();
 
   const checkingUniqueNames = (name) => {
@@ -31,61 +27,49 @@ export default function ContactForm() {
     }
   };
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    switch (name) {
-      case "name":
-        setName(value);
-        break;
-      case "number":
-        setNumber(value);
-        break;
-      default:
-        return;
-    }
-  };
-
-  const handleSubmit = () => {
-    setAddContact(name, number);
-    setName("");
-    setNumber("");
-  };
-
   const validationSchema = Yup.object().shape({
-    name: Yup.string().required("Name is required"),
-    number: Yup.string().required("Number is required"),
+    name: Yup.string()
+      .min(3, "Name must be at least 3 characters")
+      .max(20, "Name must be at most 20 characters")
+      .required("Name is required"),
+    number: Yup.string()
+      .min(3, "Number must be at least 3 characters")
+      .max(20, "Number must be at most 20 characters")
+      .required("Number is required"),
   });
+
+  const handleSubmit = (values, actions) => {
+    setAddContact(values.name, values.number);
+    actions.resetForm();
+  };
 
   return (
     <Formik
       initialValues={{ name: "", number: "" }}
       validationSchema={validationSchema}
-      onSubmit={(values, actions) => {
-        actions.resetForm();
-      }}
+      onSubmit={handleSubmit}
     >
-      <form className={css.form} onSubmit={handleSubmit}>
-        <div className={css.info}>
-          <label htmlFor="name"> Name</label>
-          <Field type="text" name="name" value={name} onChange={handleChange} />
-          <ErrorMessage
-            name="username"
-            component="span"
-            className={css.error}
-          />
-          <label htmlFor="number">Number</label>
-          <Field
-            type="tel"
-            name="number"
-            value={number}
-            onChange={handleChange}
-          />
-          <ErrorMessage name="number" component="span" className={css.error} />
-        </div>
-        <button className={css.add_button} type="submit">
-          Add contact
-        </button>
-      </form>
+      {({ handleSubmit }) => (
+        <form className={css.form} onSubmit={handleSubmit}>
+          <div className={css.info}>
+            <label htmlFor="name"> Name</label>
+            <Field type="text" name="name" />
+            <ErrorMessage name="name" component="span" className={css.error} />
+
+            <label htmlFor="number">Number</label>
+            <Field type="tel" name="number" />
+            <ErrorMessage
+              name="number"
+              component="span"
+              className={css.error}
+            />
+          </div>
+
+          <button className={css.add_button} type="submit">
+            Add contact
+          </button>
+        </form>
+      )}
     </Formik>
   );
 }
